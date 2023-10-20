10ad9c1bd707dfb0998af41334fb6040d25d9902
MaxTeselkin <tesmaxxxx@gmail.com>
2023-10-20 21:50:30 +0000

    added code for loading on device

--- supervisely_integration/serve/src/main.py
+++ supervisely_integration/serve/src/main.py
@@ -1,16 +1,21 @@
 import sys
+
 sys.path.append("/big_vision/")
 import supervisely as sly
 import os
 from dotenv import load_dotenv
 from typing import Literal
 import tensorflow as tf
-tf.compat.v1.enable_eager_execution()
+from scenic.projects.owl_vit import configs
+from scenic.projects.owl_vit import models
+from scenic.projects.owl_vit.notebooks import inference
+from scenic.model_lib.base_models import box_utils
 
 load_dotenv("supervisely_integration/serve/debug.env")
 load_dotenv(os.path.expanduser("./supervisely.env"))
 os.environ["TF_FORCE_GPU_ALLOW_GROWTH"] = "true"
 models_data_path = "./supervisely_integration/models/models_data.json"
+
 
 class OWLViTv2Model(sly.nn.inference.PromptBasedObjectDetection):
     def get_models(self):
@@ -41,9 +46,29 @@ class OWLViTv2Model(sly.nn.inference.PromptBasedObjectDetection):
             # hide GPUs from visible devices
             tf.config.set_visible_devices([], "GPU")
         # load model
-
+        selected_model = self.gui.get_checkpoint_info()["Model"]
+        if selected_model == "OWLv2 CLIP B/16 ST":
+            config = configs.owl_v2_clip_b16.get_config(init_mode="canonical_checkpoint")
+        elif selected_model == "OWLv2 CLIP B/16 ST+FT":
+            config = configs.owl_v2_clip_b16.get_config(init_mode="canonical_checkpoint")
+        elif selected_model == "OWLv2 CLIP B/16 ST/FT ens":
+            config = configs.owl_v2_clip_b16.get_config(init_mode="canonical_checkpoint")
+        elif selected_model == "OWLv2 CLIP L/14 ST":
+            config = configs.owl_v2_clip_l14.get_config(init_mode="canonical_checkpoint")
+        elif selected_model == "OWLv2 CLIP L/14 ST+FT":
+            config = configs.owl_v2_clip_l14.get_config(init_mode="canonical_checkpoint")
+        elif selected_model == "OWLv2 CLIP L/14 ST/FT ens":
+            config = configs.owl_v2_clip_l14.get_config(init_mode="canonical_checkpoint")
+        module = models.TextZeroShotDetectionModule(
+            body_configs=config.model.body,
+            objectness_head_configs=config.model.objectness_head,
+            normalize=config.model.normalize,
+            box_bias=config.model.box_bias,
+        )
+        variables = module.load_variables(config.init_from.checkpoint_path)
+        self.model = inference.Model(config, module, variables)
+        self.model.warm_up()
         # define class names
         self.class_names = ["object"]
         # list for storing box colors
         self.box_colors = []
-    
